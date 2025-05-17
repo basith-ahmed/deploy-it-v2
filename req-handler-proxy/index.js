d55bcd97f9bca69f24aa26c8250836a4c15e58ef
Basith Ahmed N A <basithahmed.dev@gmail.com>
2025-05-17 23:25:06 +0530

    feat: added reverse proxy to serve build files

---
+++ req-handler-proxy/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import httpProxy from "http-proxy";
+
+const BASE_PATH = process.env.BASE_PATH;
+
+const app = express();
+const proxy = httpProxy.createProxy()
+
+app.use((req, res) => {
+    const hostname = req.hostname
+    const subdomain = hostname.split('.')[0]
+
+    // TODO: Custom domain logic
+
+    const resolvePath = `${BASE_PATH}/${subdomain}`
+
+    return proxy.web(req, res, {target: resolvePath, changeOrigin: true}, (err) => {
+        console.error(err);
+        res.status(500).send('Proxy error');
+    })
+})
+
+app.listen(8000, () => console.log("Proxy server started."));
