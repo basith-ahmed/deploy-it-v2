19db27b35b20091e19f0c5c9f90e042c4dc31163
Basith Ahmed N A <basithahmed.dev@gmail.com>
2025-05-18 01:01:05 +0530

    feat: proxy index file to base path

--- req-handler-proxy/index.js
+++ req-handler-proxy/index.js
@@ -4,20 +4,28 @@ import httpProxy from "http-proxy";
 const BASE_PATH = process.env.BASE_PATH;
 
 const app = express();
-const proxy = httpProxy.createProxy()
+const proxy = httpProxy.createProxy();
 
 app.use((req, res) => {
-    const hostname = req.hostname
-    const subdomain = hostname.split('.')[0]
+  const hostname = req.hostname;
+  const subdomain = hostname.split(".")[0];
 
-    // TODO: Custom domain logic
+  // TODO: Custom domain logic
 
-    const resolvePath = `${BASE_PATH}/${subdomain}`
+  const resolvePath = `${BASE_PATH}/${subdomain}`;
 
-    return proxy.web(req, res, {target: resolvePath, changeOrigin: true}, (err) => {
-        console.error(err);
-        res.status(500).send('Proxy error');
-    })
-})
+  return proxy.web(req, res, { target: resolvePath, changeOrigin: true }, (err) => {
+      console.error(err);
+      res.status(500).send("Proxy error");
+  });
+});
+
+proxy.on("proxyReq", (proxyReq, req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    proxyReq.path += "index.html";
+  }
+  return proxyReq
+});
 
 app.listen(8000, () => console.log("Proxy server started."));
